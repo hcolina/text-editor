f9b4e7c39c4fb422df2999abd3c606f3b4f7cad3
hcolina <slushball420@gmail.com>
2020-07-25 09:10:54 -0500

    declare filetypes

--- text-editor.py
+++ text-editor.py
@@ -2,4 +2,6 @@ import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 def open_file():
-    filepath = askopenfilename()
+    filepath = askopenfilename(
+        filetypes = 
+    )
