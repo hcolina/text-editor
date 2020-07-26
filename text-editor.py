8745da526090e3843305cefcdf495b39fb03242d
hcolina <slushball420@gmail.com>
2020-07-26 11:48:38 -0500

    syntax

--- text-editor.py
+++ text-editor.py
@@ -32,7 +32,7 @@ window.title("Simple Text Editor")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-txt_edit = tk.Tk(window)
+txt_edit = tk.Text(window)
 fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
 btn_save = tk.Button(fr_buttons, text="Save As...", command=save_file)
